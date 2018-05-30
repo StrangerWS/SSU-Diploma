bdc8a55ee33911548c3d71a9e29af23259cdea97
Artem Dobrynin <artem_dobrynin@epam.com>
2018-05-30 17:35:21 +0400

    GENIOUS CNN!

--- src/main/java/com/strangerws/ssu/edu/textanalyzer/parser/App.java
+++ src/main/java/com/strangerws/ssu/edu/textanalyzer/parser/App.java
@@ -1,14 +1,17 @@
 package com.strangerws.ssu.edu.textanalyzer.parser;
 
 
+import com.strangerws.ssu.edu.textanalyzer.util.RectComparator;
 import javacnn.cnn.CNN;
 import javacnn.cnn.Layer;
 import javacnn.dataset.Dataset;
 import javacnn.util.ConcurenceRunner;
 import javacnn.util.Log;
+import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_imgproc;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
@@ -19,16 +22,18 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
-import static org.bytedeco.javacpp.opencv_imgproc.INTER_CUBIC;
-import static org.bytedeco.javacpp.opencv_imgproc.resize;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 
 public class App {
-    private final static String IMAGEPATH = "src\\main\\resources\\sample\\font-test-001.jpg";
+    private final static String IMAGEPATH = "src\\main\\resources\\sample\\hamlet-page-009.jpg";
     private final static int SIZE = 28;
 
     public static BufferedImage IplImageToBufferedImage(IplImage src) {
@@ -39,69 +44,21 @@ public class App {
     }
 
     public static void main(String[] args) throws IOException {
-//        App app = new App();
-//
-//        /*Load image in grayscale mode*/
-//        IplImage image = cvLoadImage(IMAGEPATH, 0);
-//        imshow("src\\main\\resources\\output\\1 - gray.jpg", new Mat(image)); // Save gray version of image*/
-//
-//        /*Binarising Image*/
-//        IplImage binimg = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
-//        cvThreshold(image, binimg, 0, 255, CV_THRESH_OTSU);
-//        imshow("src\\main\\resources\\output\\2 - binary.jpg", new Mat(binimg)); // Save binarised version of image*/
-//
-//        /*Invert image */
-//        Mat inverted = new Mat();
-//        bitwise_not(new Mat(binimg), inverted);
-//        IplImage inverimg = new IplImage(inverted);
-//        imshow("src\\main\\resources\\output\\3 - inverted.jpg", new Mat(inverimg)); // Save dilated version of image*/
-//
-//
-//        /*Dilate image to increase the thickness of each digit*/
-//        IplImage dilated = cvCreateImage(cvGetSize(inverimg), IPL_DEPTH_8U, 1);
-//        opencv_imgproc.cvDilate(inverimg, dilated, null, 1);
-//        imshow("src\\main\\resources\\output\\4 - dilated.jpg", new Mat(dilated)); // Save dilated version of image*/
-//
-//        /*Find countour */
-//        CvMemStorage storage = cvCreateMemStorage(0);
-//        CvSeq contours = new CvSeq();
-//        cvFindContours(dilated.clone(), storage, contours, Loader.sizeof(CvContour.class), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
-//        CvSeq ptr;
-//        List<Rect> rects = new ArrayList<>();
-//        for (ptr = contours; ptr != null; ptr = ptr.h_next()) {
-//            CvRect boundbox = cvBoundingRect(ptr, 1);
-//            Rect rect = new Rect(boundbox.x(), boundbox.y(), boundbox.width(), boundbox.height());
-//            rects.add(rect);
-//            cvRectangle(image, cvPoint(boundbox.x(), boundbox.y()),
-//                    cvPoint(boundbox.x() + boundbox.width(), boundbox.y() + boundbox.height()),
-//                    CV_RGB(0, 0, 0), 1, 0, 0);
-//        }
-//
-//        Mat result = new Mat(image);
-//        Collections.sort(rects, new RectComparator());
-//
-//        List<byte[]> letters = new ArrayList<>();
-//
-//        for (int i = 0; i < rects.size(); i++) {
-//            Rect rect = rects.get(i);
-//            Mat letter = new Mat(dilated).apply(rect);
-//            //copyMakeBorder(letter, letter, 10, 10, 10, 10, BORDER_CONSTANT, new Scalar(0, 0, 0, 0));
-//            resize(letter, letter, new Size(28, 28), 0, 0, INTER_CUBIC);
-//            imwrite("src\\main\\resources\\output\\letter-" + (i + 1) + "-.jpg", letter);
-//
-//            byte tmp[] = new byte[letter.cols() * letter.rows()];
-//            letter.data().get(tmp);
-//            letters.add(tmp);
-//
-//            //TODO
-//            //Set input image to mat
-//            //Train, Predict, Test refactor
-//            //Train net by mat images
-//            //Send letters by one and print it
-//        }
+        App app = new App();
+        List<byte[]> letters = new ArrayList<>();
 
-        runNet();
-//        imwrite("src\\main\\resources\\output\\result.jpg", result);//save final result
+        for (int i = 1; i <= 3; i++) {
+            double time = System.currentTimeMillis();
+            processImage(letters);
+            time = System.currentTimeMillis() - time;
+            System.out.println("Processing image time: " + time);
+
+            time = System.currentTimeMillis();
+            runNet(letters);
+            time = System.currentTimeMillis() - time;
+            System.out.println("Overall CNN work time: " + time);
+            System.out.println("===End of try "+ i + "===\n\n");
+        }
     }
 
     public static List<double[]> getTrainingData() {
@@ -127,6 +84,61 @@ public class App {
         return result;
     }
 
+    private static void processImage(List<byte[]> letters){
+        IplImage image = cvLoadImage(IMAGEPATH, 0);
+        //imshow("src\\main\\resources\\output\\1 - gray.jpg", new Mat(image)); // Save gray version of image*/
+
+        /*Binarising Image*/
+        IplImage binimg = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+        cvThreshold(image, binimg, 0, 255, CV_THRESH_OTSU);
+        //imshow("src\\main\\resources\\output\\2 - binary.jpg", new Mat(binimg)); // Save binarised version of image*/
+
+        /*Invert image */
+        Mat inverted = new Mat();
+        bitwise_not(new Mat(binimg), inverted);
+        IplImage inverimg = new IplImage(inverted);
+        //imshow("src\\main\\resources\\output\\3 - inverted.jpg", new Mat(inverimg)); // Save dilated version of image*/
+
+
+        /*Dilate image to increase the thickness of each digit*/
+        //IplImage dilated = cvCreateImage(cvGetSize(inverimg), IPL_DEPTH_8U, 1);
+        //opencv_imgproc.cvDilate(inverimg, dilated, null, 1);
+        //imshow("src\\main\\resources\\output\\4 - dilated.jpg", new Mat(dilated)); // Save dilated version of image*/
+
+        /*Find countour */
+        CvMemStorage storage = cvCreateMemStorage(0);
+        CvSeq contours = new CvSeq();
+        cvFindContours(inverimg.clone(), storage, contours, Loader.sizeof(CvContour.class), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+        CvSeq ptr;
+        List<Rect> rects = new ArrayList<>();
+        for (ptr = contours; ptr != null; ptr = ptr.h_next()) {
+            CvRect boundbox = cvBoundingRect(ptr, 1);
+            Rect rect = new Rect(boundbox.x(), boundbox.y(), boundbox.width(), boundbox.height());
+            rects.add(rect);
+            cvRectangle(image, cvPoint(boundbox.x(), boundbox.y()),
+                    cvPoint(boundbox.x() + boundbox.width(), boundbox.y() + boundbox.height()),
+                    CV_RGB(0, 0, 0), 1, 0, 0);
+        }
+
+        Mat result = new Mat(image);
+        Collections.sort(rects, new RectComparator());
+
+
+
+        for (int i = 0; i < rects.size(); i++) {
+            Rect rect = rects.get(i);
+            Mat letter = new Mat(inverimg).apply(rect);
+            //copyMakeBorder(letter, letter, 10, 10, 10, 10, BORDER_CONSTANT, new Scalar(0, 0, 0, 0));
+            resize(letter, letter, new Size(28, 28), 0, 0, INTER_CUBIC);
+            bitwise_not(letter, letter);
+            //imwrite("src\\main\\resources\\output\\letter-" + (i + 1) + ".jpg", letter);
+
+            byte tmp[] = new byte[letter.cols() * letter.rows()];
+            letter.data().get(tmp);
+            letters.add(tmp);
+        }
+    }
+
     private static double[] doubleCast(byte[] tmp) {
         double[] casted = new double[tmp.length];
         for (int i = 0; i < tmp.length; i++) {
@@ -147,8 +159,9 @@ public class App {
         return result;
     }
 
-    private static void runNet() {
+    private static void runNet(List<byte[]> images) {
         Dataset dataset = new Dataset();
+        Dataset test = new Dataset();
 
         final ConcurenceRunner concurenceRunner = new ConcurenceRunner();
         CNN.LayerBuilder builder = new CNN.LayerBuilder();
@@ -159,9 +172,18 @@ public class App {
         builder.addLayer(Layer.buildSampLayer(new javacnn.cnn.Layer.Size(2, 2)));
         builder.addLayer(Layer.buildOutputLayer(27));
         CNN cnn = new CNN(builder, 50, concurenceRunner);
-        setDataset(dataset);
-        Log.switchOn();
-        cnn.train(dataset, 100);
+        setTrainDatasetFromRes(dataset);
+        setPredictDatasetFromList(test, images);
+        //Log.switchOn();
+        double time = System.currentTimeMillis();
+        cnn.train(dataset, 10);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Training CNN time: " +time);
+
+        time = System.currentTimeMillis();
+        cnn.predict(test, "src/main/resources/output/tested.txt");
+        time = System.currentTimeMillis() - time;
+        System.out.println("Predicting CNN time: " +time);
 
     }
 
@@ -173,27 +195,43 @@ public class App {
     }
 
 
-    private static void setDataset(Dataset dataset) {
-        String pathBegin = "src\\main\\resources\\trainingData\\";
-        StringBuilder path;
+    private static void setTrainDatasetFromRes(Dataset dataset) {
+
         for (int l = 0; l < 10; l++) {
             for (int i = 1; i <= 26; i++) {
                 for (int j = 1; j <= 30; j++) {
-                    path = new StringBuilder(pathBegin)
-                            .append(i)
-                            .append("\\")
-                            .append(j)
-                            .append(".png");
-                    byte[] tmp = new byte[SIZE * SIZE];
-                    Mat mat = new Mat(cvLoadImage(path.toString(), CV_LOAD_IMAGE_GRAYSCALE));
-                    resize(mat, mat, new opencv_core.Size(SIZE, SIZE), 0, 0, INTER_CUBIC);
-                    mat.data().get(tmp);
-                    dataset.append(doubleCast(tmp), (double) i);
+                    dataset.append(getTrainingImage(i, j), (double) i);
+                }
+            }
+            for (int i = 27; i <= 52; i++) {
+                for (int j = 1; j <= 30; j++) {
+                    dataset.append(getTrainingImage(i, j), (double)i - 26);
                 }
             }
         }
 
         dataset.shuffle();
+    }
+
+    private static double[] getTrainingImage(int i, int j){
+        String pathBegin = "src\\main\\resources\\trainingData\\";
+        StringBuilder path;
+        path = new StringBuilder(pathBegin)
+                .append(i)
+                .append("\\")
+                .append(j)
+                .append(".png");
+        byte[] tmp = new byte[SIZE * SIZE];
+        Mat mat = new Mat(cvLoadImage(path.toString(), CV_LOAD_IMAGE_GRAYSCALE));
+        resize(mat, mat, new opencv_core.Size(SIZE, SIZE), 0, 0, INTER_CUBIC);
+        mat.data().get(tmp);
+        return doubleCast(tmp);
+    }
+
+    private static void setPredictDatasetFromList(Dataset dataset, List<byte[]> images){
+        for (byte[] image : images) {
+            dataset.append(doubleCast(image), null);
+        }
     }
 
 }
--- src/main/java/com/strangerws/ssu/edu/textanalyzer/util/RectComparator.java
+++ src/main/java/com/strangerws/ssu/edu/textanalyzer/util/RectComparator.java
@@ -7,6 +7,6 @@ public class RectComparator implements Comparator<opencv_core.Rect> {
 
     @Override
     public int compare(opencv_core.Rect t1, opencv_core.Rect t2) {
-        return Integer.valueOf(t1.x()).compareTo(t2.x());
+        return Integer.valueOf(t1.y()).compareTo(t2.y());
     }
 }
